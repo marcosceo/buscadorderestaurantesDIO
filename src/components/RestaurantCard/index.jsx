b6a5d73b3f7f9a160d17d5459706e89d81744beb
Marcos <marcos.ceo@outlook.com>
2021-11-09 21:11:29 -0300

    Projeto finalizado

---
+++ src/components/RestaurantCard/index.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import ReactStars from 'react-rating-stars-component';
+
+import restaurante from '../../assets/restaurante-fake.png';
+import Skeleton from '../Skeleton';
+
+import { Address, Restaurant, RestaurantInfo, RestaurantPhoto, Title } from './styles';
+
+const RestaurantCard = ({ restaurant, onClick }) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  return (
+    <Restaurant onClick={onClick}>
+      <RestaurantInfo>
+        <Title>{restaurant.name}</Title>
+        <ReactStars count={restaurant.rating} isHalf value={4} edit={false} activeColor="#e7711c" />
+        <Address>{restaurant.vicinity || restaurant.formatted_address}</Address>
+      </RestaurantInfo>
+      <RestaurantPhoto
+        imageLoaded={imageLoaded}
+        src={restaurant.photos ? restaurant.photos[0].getUrl() : restaurante}
+        onLoad={() => setImageLoaded(true)}
+        alt="Foto do restaurante"
+      />
+      {!imageLoaded && <Skeleton width="100px" height="100px" />}
+    </Restaurant>
+  );
+};
+
+export default RestaurantCard;
